3dd396f0897055b8c959f59246a8c69f28a14876
ProSachi <syosa@cesde.net>
2025-12-13 14:56:40 -0500

    guías de clase

--- 13122025/ayer/main.js
+++ 13122025/ayer/main.js
@@ -1,4 +1,4 @@
-const usuarioCorrecto = "Santiago";
+const usuarioCorrecto = "santiago";
 const contrasenaCorrecto = "12345";
 
 const nombre = document.getElementById('name')
@@ -8,23 +8,15 @@ const quiero = document.getElementById('btn')
 quiero.addEventListener('click', () => {
     const namecapture = nombre.value.trim();
     const contracapture = contrasena.value.trim();
+    validar(namecapture, contracapture)
 });
 
-function validar (namecapture, contracapture) {
+function validar(namecapture, contracapture) {
 
-    if (namecapture === usuarioCorrecto && contracapture===contrasenaCorrecto) {
+    if (namecapture === usuarioCorrecto && contracapture === contrasenaCorrecto) {
         alert("Bienvenido")
     } else {
-         alert("Datos Incorrectos")
+        alert("Datos Incorrectos")
     }
-  
+
 }
-
-/* const nombre = document.querySelector('#name') */
-const nombre2 = document.querySelector('.input')
-console.log(nombre2);
-
-quiero.addEventListener('click', () => {
-    const namecapture = nombre2.value.trim();
-    console.log(namecapture);
-});
